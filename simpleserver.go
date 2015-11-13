66b701b8c1e494a878c25662a938994cc3b43e03
Anthony Eden <me@anthonyeden.com>
2015-11-13 14:22:38 +0100

    Echo the request and provide paths for custom response codes.

--- simpleserver.go
+++ simpleserver.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"flag"
+	"fmt"
 	"io/ioutil"
 	"log"
 	"net"
@@ -37,24 +38,67 @@ func router() http.Handler {
 		log.Printf("GET /")
 	})
 	router.POST("/", func(res http.ResponseWriter, req *http.Request, params httprouter.Params) {
-		data, err := ioutil.ReadAll(req.Body)
-		if err != nil {
-			log.Printf("Error: %v", err)
-		} else {
-			log.Printf("Data: %v", string(data))
-		}
+		log.Printf("POST /")
+		echo(res, req, http.StatusOK)
 	})
 	router.PUT("/", func(res http.ResponseWriter, req *http.Request, params httprouter.Params) {
-		data, err := ioutil.ReadAll(req.Body)
-		if err != nil {
-			log.Printf("Error: %v", err)
-		} else {
-			log.Printf("Data: %v", string(data))
-		}
+		log.Printf("PUT /")
+		echo(res, req, http.StatusOK)
 	})
 	router.DELETE("/", func(res http.ResponseWriter, req *http.Request, params httprouter.Params) {
 		log.Printf("DELETE /")
+		res.WriteHeader(http.StatusNoContent)
+	})
+
+	router.GET("/:code", func(res http.ResponseWriter, req *http.Request, params httprouter.Params) {
+		log.Printf("GET /%v", params.ByName("code"))
+		code, err := strconv.Atoi(params.ByName("code"))
+		if err != nil {
+			http.Error(res, fmt.Sprintf("Error converting requested status code to int: %v", err), http.StatusBadRequest)
+		} else {
+			res.WriteHeader(code)
+		}
+	})
+	router.POST("/:code", func(res http.ResponseWriter, req *http.Request, params httprouter.Params) {
+		log.Printf("POST /%v", params.ByName("code"))
+		code, err := strconv.Atoi(params.ByName("code"))
+		if err != nil {
+			http.Error(res, fmt.Sprintf("Error converting requested status code to int: %v", err), http.StatusBadRequest)
+		} else {
+			echo(res, req, code)
+		}
+	})
+	router.PUT("/:code", func(res http.ResponseWriter, req *http.Request, params httprouter.Params) {
+		log.Printf("PUT /%v", params.ByName("code"))
+		code, err := strconv.Atoi(params.ByName("code"))
+		if err != nil {
+			http.Error(res, fmt.Sprintf("Error converting requested status code to int: %v", err), http.StatusBadRequest)
+		} else {
+			echo(res, req, code)
+		}
+	})
+	router.DELETE("/:code", func(res http.ResponseWriter, req *http.Request, params httprouter.Params) {
+		log.Printf("DELETE /%v", params.ByName("code"))
+		code, err := strconv.Atoi(params.ByName("code"))
+		if err != nil {
+			http.Error(res, fmt.Sprintf("Error converting requested status code to int: %v", err), http.StatusBadRequest)
+		} else {
+			res.WriteHeader(code)
+		}
 	})
 
 	return router
+}
+
+func echo(res http.ResponseWriter, req *http.Request, code int) {
+	defer req.Body.Close()
+	data, err := ioutil.ReadAll(req.Body)
+	if err != nil {
+		log.Printf("Error: %v", err)
+	} else {
+		log.Printf("Data: %v", string(data))
+	}
+	res.Header().Add("Content-type", req.Header.Get("Content-type"))
+	res.WriteHeader(code)
+	res.Write(data)
 }
